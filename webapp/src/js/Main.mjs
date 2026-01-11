ac2b764c5cbd2bcdf02d96ba382eacb4ed7199df
Sofotschka <161605502+Sofotschka@users.noreply.github.com>
2026-01-11 16:07:15 +0100

    Added a function to the buttons to display the different bodies

--- webapp/src/js/Main.mjs
+++ webapp/src/js/Main.mjs
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
   if (app) app.textContent = 'Hello World';
 });
+
+const bodyBetreiber = document.getElementByID("Betreiber");
+const bodyKunde = document.getElementByID("Kunde");
+
+document.getElementById("BetreiberButton").addEventListener("click", () => {
+  bodyBetreiber.style.display = "block";
+  bodyKunde.style.display = "none";
+});
+
+document.getElementById("KundenButton").addEventListener("click", () => {
+  bodyKunde.style.display = "block";
+  bodyBetreiber.style.display = "none";
+});
